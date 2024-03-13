0d9ac00550c352452774c5a05cabbd5567d5968a
Inscription404 <162913621+Inscription404@users.noreply.github.com>
2024-03-13 15:04:25 +0800

    Create example.sol

---
+++ example.sol
@@ -0,0 +1,99 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@5.0.2/access/Ownable.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Permit.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+
+interface Inscription404 {
+    event TransferToken(
+        address indexed from,
+        address indexed to,
+        uint256 amount,
+        uint256 decimal
+    );
+
+    event TransferNFT(
+        address indexed from,
+        address indexed to,
+        uint256 amount,
+        uint256 tokenId,
+        uint8 decimal
+    );
+
+    // function deploy(
+    //     uint8 d
+    // ) external;
+
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+
+    function transferToken(uint256 tokenID, address recipient)
+        external
+        returns (bool);
+
+    function transferTokenFrom(
+        uint256 tokenID,
+        address sender,
+        address recipient
+    ) external returns (bool);
+}
+
+contract MyToken is ERC20, ERC20Burnable, Ownable, ERC20Permit, Inscription404 {
+    using Strings for uint256;
+
+    constructor(address initialOwner)
+        ERC20("MyToken", "MTK")
+        Ownable(initialOwner)
+        ERC20Permit("MyToken")
+    {}
+    string public baseURI = "";
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+                return    bytes(baseURI).length > 0
+                ? string.concat(baseURI, tokenId.toString())
+                : "";
+    }
+
+    function transferToken(uint256 tokenID, address recipient)
+        external
+        returns (bool)
+    {
+        uint256 amount = 1 * 10**decimals();
+        address owner = _msgSender();
+
+        // transfer(sender, recipient, amount);
+
+        ERC20.transfer(recipient, amount);
+        emit TransferNFT(owner, recipient, amount, tokenID, decimals());
+        return true;
+    }
+
+    function transferTokenFrom(
+        uint256 tokenID,
+        address sender,
+        address recipient
+    ) external returns (bool) {
+        uint256 amount = 1 * 10**decimals();
+        // address owner = _msgSender();
+
+        ERC20.transferFrom(sender, recipient, amount);
+        emit TransferNFT(sender, recipient, amount, tokenID, decimals());
+        return true;
+    }
+
+    function _update(
+        address from,
+        address to,
+        uint256 value
+    ) internal virtual override {
+        ERC20._update(from, to, value);
+        emit TransferToken(from, to, value, decimals());
+    }
+}
